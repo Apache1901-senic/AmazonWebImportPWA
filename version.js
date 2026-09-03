ee428bdbab938a2b80e5f927de258efab731c8ca
Apache1901-senic <stephan.hummel.privat@gmail.com>
2026-09-03 08:31:35 +0200

    Hilfe, Changelog und Sicherungsfunktion auf Version 1.2.1 aktualisiert

--- version.js
+++ version.js
@@ -8,14 +8,56 @@ const APP_INFO = {
 
     name: "Amazon CSV Importer",
 
-    version: "1.0.1",
+    version: "1.2.1",
 
-    buildDate: "2026-08-26",
+    buildDate: "2026-09-03",
 
     author: ".::ST:HU::.",
 
     changelog: [
-         {
+
+        {
+            version: "1.2.1",
+            date: "2026-09-03",
+            changes: [
+                "Sicherungsabfrage vor dem JSON-Import hinzugefügt",
+                "Direkte JSON-Sicherung vor dem Ersetzen der Produktdaten möglich",
+                "Hilfe und Programmdokumentation vollständig überarbeitet",
+                "Dokumentation für Offline-Betrieb und automatische Synchronisation ergänzt",
+                "Dokumentation für Mehr-PC-Betrieb und Konfliktbehandlung ergänzt"
+            ]
+        },
+
+        {
+            version: "1.2.0",
+            date: "2026-09-02",
+            changes: [
+                "Live-Synchronisation zwischen mehreren Computern integriert",
+                "Sichere Erkennung paralleler Produktänderungen implementiert",
+                "Konfliktfenster für unterschiedliche lokale und zentrale Produktdaten hinzugefügt",
+                "Auswahl zwischen lokalem und zentralem Datenstand bei Konflikten",
+                "Sichere Versionierung des zentralen Produktdatenbestands bei gleichzeitigem Zugriff mehrerer Computer",
+                "Sicherer Server-Abgleich nach dem Offline-Betrieb integriert",
+                "Automatische Wiederaufnahme der Synchronisation nach Wiederherstellung der Verbindung",
+                "Build-Kennung zur Kontrolle des veröffentlichten Programmstands ergänzt",
+                "Mehr-PC- und Offline-Konflikttests erfolgreich abgeschlossen"
+            ]
+        },
+
+        {
+            version: "1.1.0",
+            date: "2026-08-28",
+            changes: [
+                "Zentrale Produktdaten über Firebase Firestore integriert",
+                "Firebase-Anmeldung und Abmeldung hinzugefügt",
+                "Automatische Synchronisation der Produktdaten zwischen mehreren Computern",
+                "Offline-Änderungen werden lokal gespeichert und später synchronisiert",
+                "Synchronisationsstatus und Benutzerhinweise ergänzt",
+                "Produktdaten-Versionierung für den zentralen Datenbestand eingeführt"
+            ]
+        },
+
+        {
             version: "1.0.1",
             date: "2026-08-26",
             changes: [
